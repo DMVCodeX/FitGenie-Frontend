8b7a686130dc29a5bfa8e9ed01d34ca5250d4fa2
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-20 21:18:46 -0400

    Add props and map through fake data stored in content

--- src/Usersindex.jsx
+++ src/Usersindex.jsx
@@ -1,7 +1,14 @@
-export function UsersIndex() {
+export function UsersIndex(props) {
   return (
     <div>
-      <h1>All Users</h1>
+      {props.users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <img src={user.url} />
+          <p>Width: {user.width}</p>
+          <p>Height: {user.height}</p>
+        </div>
+      ))}
     </div>
   );
 }
