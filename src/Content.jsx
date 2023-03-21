263b3e9042d9a332a13cb86555cef047e4371f2b
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-20 21:08:34 -0400

    Add place holder data in content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,14 @@
 import { UsersIndex } from "./Usersindex";
 
 export function Content() {
+  const users = [
+    { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
+    { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+  ];
+
   return (
     <div>
-      <UsersIndex />
+      <UsersIndex users={users} />
     </div>
   );
 }
