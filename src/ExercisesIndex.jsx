52961554f67de054a98adfa7766353c53b3bad11
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-28 14:36:11 -0400

    change exercise images

--- src/ExercisesIndex.jsx
+++ src/ExercisesIndex.jsx
@@ -2,22 +2,26 @@ import ReactPlayer from "react-player";
 
 export function ExercisesIndex(props) {
   return (
-    <div className="row row-cols-1 row-cols-md-2 g-4">
-      {props.exercises.map((exercise) => (
-        <div className="card" key={exercise.id}>
-          <h2 className="card-title">{exercise.name}</h2>
-          <img className="card-img-top" src={exercise.image_url} />
-          <p></p>
-          <p className="text-line-break">{exercise.description}</p>
-          <p>Suggested reps: {exercise.reps}</p>
-          <ReactPlayer className="centered-image" url={exercise.video_url} />
-          <p></p>
-          <button type="submit" className="btn btn-outline-secondary">
-            Add to Workout
-          </button>
-          <p></p>
-        </div>
-      ))}
+    <div>
+      <h1>Explore Exercises</h1>
+      <p></p>
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {props.exercises.map((exercise) => (
+          <div className="card" key={exercise.id}>
+            <h2 className="card-title">{exercise.name}</h2>
+            <img className="card-img-top" src={exercise.image_url} />
+            <p></p>
+            <p className="text-line-break">{exercise.description}</p>
+            <p>Suggested reps: {exercise.reps}</p>
+            <ReactPlayer className="centered-image" url={exercise.video_url} />
+            <p></p>
+            <button type="submit" className="btn btn-outline-secondary">
+              Add to Workout
+            </button>
+            <p></p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
