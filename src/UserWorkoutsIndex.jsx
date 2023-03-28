303fab0f355d1f4a1472648a5f925232418e448a
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-28 01:11:39 -0400

    add accordian styling in userworkoutsindex

--- src/UserWorkoutsIndex.jsx
+++ src/UserWorkoutsIndex.jsx
@@ -1,30 +1,39 @@
+import ReactPlayer from "react-player";
+
 export function UserWorkoutsIndex(props) {
   return (
     <div>
-      <h1>User Workouts Go Here</h1>
-      <p>{props.user.name}</p>
+      <p></p>
+      <h1>{props.user.name}'s Workouts</h1>
+      <p></p>
+
       <div class="accordion" id="accordionExample">
         {props.user.workouts.map((workout) => (
           <div class="accordion-item">
             <h2 class="accordion-header">
               <button
-                class="accordion-button collapsed"
+                className="btn btn-outline-secondary container-fluid"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target={"#collapseTwo" + workout.id}
                 aria-expanded="false"
                 aria-controls="collapseTwo"
               >
+                <p></p>
                 {workout.name}
+                <p></p>
               </button>
             </h2>
             <div id={"collapseTwo" + workout.id} class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div class="accordion-body">
                 {workout.workout_exercises.map((workoutExercise) => (
                   <div>
-                    <p>{workoutExercise.exercise.name}</p>
-                    <img src={workoutExercise.exercise.image_url} alt="" />
+                    <p></p>
+                    <h2 className="center-text">{workoutExercise.exercise.name}</h2>
+                    <img className="centered-image" src={workoutExercise.exercise.image_url} alt="" />
                     <p className="text-line-break">{workoutExercise.exercise.description}</p>
+                    <ReactPlayer className="centered-image" url={workoutExercise.exercise.video_url} />
+                    <p></p>
                   </div>
                 ))}
               </div>
