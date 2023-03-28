37813579e71cc3bfef503e57f40d7b440f46f346
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-28 18:19:33 -0400

    add footer alignment

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,6 @@
 export function Footer() {
   return (
-    <footer className="container-sm">
-      <p></p>
+    <footer className="container-sm center-text">
       <p></p>
       <p>Copyright 2023</p>
     </footer>
