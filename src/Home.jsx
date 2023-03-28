d1088bf8385dc7218df3c5fbb4e016946467f71f
DIANELLY VENTURA <DMVCode@outlook.com>
2023-03-28 04:15:49 -0400

    styling to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
 export function Home() {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid head">
       <head>
         <style>@import url('https://fonts.googleapis.com/css2?family=Exo+2:wght@100&display=swap');</style>
       </head>
+      <p></p>
+      <h1>Endurance | Determination | Consistency </h1>
       <p></p>
 
       <p></p>
