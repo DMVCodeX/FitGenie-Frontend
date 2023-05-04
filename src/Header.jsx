8a931c1cd95db03625f6edc57314fcfe5024da92
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-03 21:02:11 -0400

    navbar updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,8 +12,8 @@ export function Header(props) {
           </a>
         </div>
       </nav>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary container-fluid ">
+        <div className="container">
           <ul className="nav justifycontent-center">
             <li> </li>
             <li className="nav-item">
@@ -117,76 +117,88 @@ export function Header(props) {
               </a>
             </li>
           </ul>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div
-            class="offcanvas offcanvas-end"
-            tabindex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title center-text" id="offcanvasNavbarLabel">
-                Dashboard
-              </h5>
-              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/home">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item ml-6 ">
-                  Logout <LogoutLink />
-                </li>
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Profile Settings
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-secondary" type="submit">
-                  Search
-                </button>
-              </form>
+          <div className="">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+              class="offcanvas offcanvas-end"
+              tabindex="-1"
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
+              <div class="offcanvas-header">
+                <h5 class="offcanvas-title center-text" id="offcanvasNavbarLabel">
+                  Dashboard
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/home">
+                      Home
+                    </a>
+                  </li>
+
+                  <li class="nav-item dropdown">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Current User
+                      {/* <img
+                        src="https://img.icons8.com/?size=512&id=15263&format=png"
+                        alt="Logo"
+                        width="25"
+                        height="25"
+                        className="d-inline-block align-text-top m-1"
+                      /> */}
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Account Settings
+                          <img
+                            src="https://img.icons8.com/?size=512&id=364&format=png"
+                            alt="Logo"
+                            width="25"
+                            height="25"
+                            className="d-inline-block align-text-top m-1"
+                          />
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          About Us
+                        </a>
+                      </li>
+                      <li>
+                        <hr class="dropdown-divider" />
+                      </li>
+                      <li className="dropdown-item ml-6 ">
+                        Logout <LogoutLink />
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex mt-3" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                  <button class="btn btn-outline-secondary" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
