d31cd25f5a9507957c04e58990ca16d27788b1e3
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-05 16:21:44 -0400

    add geo loaction to map

--- src/Maps.jsx
+++ src/Maps.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { Map, Marker } from "pigeon-maps";
+import { useState, useEffect } from "react";
 
 export function Maps() {
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation([latitude, longitude]);
+      });
+    }
+  }, []);
+
   return (
     <div className="">
       <h1>Find a Gym Near You</h1>
-      <Map height={500} defaultCenter={[26.10812, -80.25222]} defaultZoom={17}>
+
+      {userLocation && (
+        <Map height={500} center={userLocation} zoom={10}>
+          <Marker anchor={userLocation} />
+        </Map>
+      )}
+
+      {/* <Map height={500} defaultCenter={[26.10812, -80.25222]} defaultZoom={17}>
         <Marker width={50} anchor={[26.10812, -80.25222]} />
-      </Map>
+      </Map> */}
     </div>
   );
 }
